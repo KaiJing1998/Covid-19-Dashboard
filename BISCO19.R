2375b49bb409959167b6cafda599f57210d4d747
KaiJing1998 <66261593+KaiJing1998@users.noreply.github.com>
2021-06-06 00:05:38 +0800

    Update

--- BISCO19.R
+++ BISCO19.R
@@ -23,6 +23,23 @@ mydb <- dbConnect(MySQL(),
 dbListTables(mydb)
 
 
+
+dbListFields(mydb,'DAILYCASES')
+rs01 = dbSendQuery(mydb, "SELECT `Daily New Cases` FROM DAILYCASES ORDER BY `Date` DESC LIMIT 1")
+dailynewcases = fetch (rs01, n= -1)
+
+dbListFields(mydb,'DAILYCASES')
+rs02 = dbSendQuery(mydb, "SELECT `Daily Deaths` FROM DAILYCASES ORDER BY `Date` DESC LIMIT 1")
+dailydeaths = fetch (rs02, n= -1)
+
+dbListFields(mydb,'DAILYCASES')
+rs03 = dbSendQuery(mydb, "SELECT `Daily Recovery` FROM DAILYCASES ORDER BY `Date` DESC LIMIT 1")
+dailyrecovery = fetch (rs03, n= -1)
+
+dbListFields(mydb,'DAILYCASES')
+rs04 = dbSendQuery(mydb, "SELECT `Active Cases` FROM DAILYCASES ORDER BY `Date` DESC LIMIT 1")
+activecases = fetch (rs04, n= -1)
+
 dbListFields(mydb,'DAILYCASES')
 rs = dbSendQuery(mydb, "SELECT * FROM DAILYCASES")
 datatable = fetch (rs, n= -1)
@@ -234,14 +251,14 @@ ui <- dashboardPage(
     fluidRow(
       fluidPage(
       "updated",
-      em("May 28, 2021"),
+      em("June 5, 2021"),
       br(),
       
       # A static value Box
-      valueBox("7,857","New Cases", color = "red", width = 3, icon = icon("arrow-alt-circle-up")),
-      valueBox("59","New Deaths", color = "olive",width = 3, icon = icon("arrow-alt-circle-up")),
-      valueBox("4,598","New Recovered", color = "light-blue",width = 3, icon = icon("arrow-alt-circle-up")),
-      valueBox("69,408","Active Cases", color = "navy", width = 3, icon = icon("arrow-alt-circle-up")),
+      valueBox(dailynewcases,"New Cases", color = "red", width = 3, icon = icon("arrow-alt-circle-up")),
+      valueBox(dailydeaths,"New Deaths", color = "olive",width = 3, icon = icon("arrow-alt-circle-up")),
+      valueBox(dailyrecovery,"New Recovered", color = "light-blue",width = 3, icon = icon("arrow-alt-circle-up")),
+      valueBox(activecases,"Active Cases", color = "navy", width = 3, icon = icon("arrow-alt-circle-up")),
       )
     ),
    
@@ -303,8 +320,8 @@ ui <- dashboardPage(
       tabItem("kualalumpur",
               fluidPage(
                 h2("Kuala Lumpur"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("kualaLumpurDistrict"),
                 br(),
@@ -315,8 +332,8 @@ ui <- dashboardPage(
       tabItem("selangor",
               fluidPage(
                 h2("Selangor"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("selangorDistrict"),
                 br(),
@@ -327,8 +344,8 @@ ui <- dashboardPage(
       tabItem("johor",
               fluidPage(
                 h2("Johor"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("johorDistrict"),
                 br(),
@@ -339,8 +356,8 @@ ui <- dashboardPage(
       tabItem("perlis",
               fluidPage(
                 h2("Perlis"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("perlisDistrict"),
                 br(),
@@ -351,8 +368,8 @@ ui <- dashboardPage(
       tabItem("penang",
               fluidPage(
                 h2("Penang"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("penangDistrict"),
                 br(),
@@ -363,8 +380,8 @@ ui <- dashboardPage(
       tabItem("kedah",
               fluidPage(
                 h2("Kedah"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("kedahDistrict"),
                 br(),
@@ -375,8 +392,8 @@ ui <- dashboardPage(
       tabItem("kelantan",
               fluidPage(
                 h2("Kelantan"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("kelantanDistrict"),
                 br(),
@@ -387,8 +404,8 @@ ui <- dashboardPage(
       tabItem("terengganu",
               fluidPage(
                 h2("Terengganu"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("terengganuDistrict"),
                 br(),
@@ -399,8 +416,8 @@ ui <- dashboardPage(
       tabItem("melaka",
               fluidPage(
                 h2("Melaka"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("melakaDistrict"),
                 br(),
@@ -411,8 +428,8 @@ ui <- dashboardPage(
       tabItem("pahang",
               fluidPage(
                 h2("Pahang"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("pahangDistrict"),
                 br(),
@@ -423,8 +440,8 @@ ui <- dashboardPage(
       tabItem("labuan",
               fluidPage(
                 h2("Labuan"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("labuanDistrict"),
                 br(),
@@ -436,8 +453,8 @@ ui <- dashboardPage(
       tabItem("putrajaya",
               fluidPage(
                 h2("Putrajaya"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("putrajayaDistrict"),
                 br(),
@@ -448,8 +465,8 @@ ui <- dashboardPage(
       tabItem("negerisembilan",
               fluidPage(
                 h2("Negeri Sembilan"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("nsembilanDistrict"),
                 br(),
@@ -460,8 +477,8 @@ ui <- dashboardPage(
       tabItem("sabah",
               fluidPage(
                 h2("Sabah"),
-                #"updated",
-                #em("May 16, 2021"),
+                "updated",
+                em("May 16, 2021"),
                 br(),
                 plotlyOutput("sabahDistrict"),
                 br(),
